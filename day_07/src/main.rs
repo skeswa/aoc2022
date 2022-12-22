c620012021eff6fef9e2df705bff9d56deff16b5
Sandile Keswa <me@sandile.io>
2022-12-22 05:25:33 -0500

    day_07: part 2 is done and dusted

--- day_07/src/main.rs
+++ day_07/src/main.rs
@@ -45,16 +45,50 @@ async fn main() -> Result<()> {
     let file_system = FileSystem::build_imperatively(command_invocations)
         .context("Failed to assemble file system from imperative commands")?;
 
-    let total_size_of_slender_directories = file_system
-        .sizes()
-        .iter()
-        .filter(|(node, size)| node.is_directory() && *size <= 100000)
-        .fold(0, |acc, (_, size)| acc + *size);
+    let mut file_system_sizes = file_system.sizes();
 
-    println!(
-        "Total size of all directories smaller than 100000: {}",
-        total_size_of_slender_directories
-    );
+    match config.part {
+        1 => {
+            let total_size_of_slender_directories = file_system_sizes
+                .iter()
+                .filter(|(node, size)| node.is_directory() && *size <= 100000)
+                .fold(0, |acc, (_, size)| acc + *size);
+
+            println!(
+                "Total size of all directories smaller than 100000: {}",
+                total_size_of_slender_directories
+            );
+        }
+        _ => {
+            let total_size = file_system_sizes
+                .iter()
+                .rev()
+                .filter(|(node, _)| node.name == "/")
+                .map(|(_, size)| *size)
+                .next()
+                .context("Failed to derive \"/\" size total")?;
+            println!("Total size: {}", total_size);
+
+            let remaining_space = 70_000_000 - total_size;
+            println!("Remaining space: {}", remaining_space);
+
+            file_system_sizes.sort_by(|a, b| a.1.cmp(&b.1));
+
+            let size_of_directory_to_delete = file_system_sizes
+                .iter()
+                .filter(|(node, size)| {
+                    node.is_directory() && (remaining_space + *size) >= 30_000_000
+                })
+                .map(|(_, size)| *size)
+                .next()
+                .context("Could find a directory to delete")?;
+
+            println!(
+                "We can delete directory with size: {}",
+                size_of_directory_to_delete
+            );
+        }
+    }
 
     Ok(())
 }
